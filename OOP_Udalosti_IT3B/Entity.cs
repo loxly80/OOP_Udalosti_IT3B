cb5422e53c6711d6c98c978631f58ae8daaf31ca
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-11-20 10:29:45 +0100

    Přidejte soubory projektu.

---
+++ OOP_Udalosti_IT3B/Entity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace OOP_Udalosti_IT3B
+{
+  public class Entity
+  {
+    private int selectedColor;
+
+    public Point Location { get; set; }
+    public double Size { get; set; }
+    public Brush Color { get => Colors[selectedColor]; }
+    public List<Brush> Colors { get; set; }
+
+    public Entity()
+    {
+      Random random = new Random();
+      Location = new Point();
+      Size = 50;
+      Colors = new List<Brush>() {
+       Brushes.White,
+       Brushes.Red,
+       Brushes.Green,
+       Brushes.Blue,
+       Brushes.Yellow,
+       Brushes.Cyan,
+       Brushes.Magenta,
+      };
+      selectedColor = random.Next(0, Colors.Count);
+    }
+  }
+}
---
+++ OOP_Udalosti_IT3B/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_Udalosti_IT3B
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    List<Entity> entities = new List<Entity>();
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      GenerateEntities();
+      DrawEntities();
+    }
+
+    private void DrawEntities()
+    {
+      Canvas1.Children.Clear();
+      foreach (var entity in entities)
+      {
+        var rectangle = new Rectangle();
+        rectangle.Width = entity.Size;
+        rectangle.Height = entity.Size;
+        rectangle.Fill = entity.Color;
+        rectangle.Stroke = Brushes.Black;
+        rectangle.StrokeThickness = 3;
+        Canvas.SetLeft(rectangle, entity.Location.X);
+        Canvas.SetTop(rectangle, entity.Location.Y);
+        Canvas1.Children.Add(rectangle);
+      }
+    }
+
+    private void GenerateEntities()
+    {
+      Random random = new Random();
+      for (int i = 0; i < 10; i++)
+      {
+        var entity = new Entity();
+        entity.Size = random.NextDouble() * 50 + 20;
+        var x = random.NextDouble() * (Canvas1.ActualWidth - entity.Size);
+        var y = random.NextDouble() * (Canvas1.ActualHeight - entity.Size);
+        entity.Location = new Point(x, y);
+        entities.Add(entity);
+      }
+    }    
+  }
+}
